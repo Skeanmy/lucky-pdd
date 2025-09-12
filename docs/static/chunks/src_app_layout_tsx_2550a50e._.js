748395b3b9224c97ceeb9586e0b6a17d216a1cc3
YifNan <nanyifei@modelbest.cn>
2025-09-12 16:32:50 +0800

    删除多个旧的构建和清单文件，更新文档结构，优化静态资源管理。

--- docs/static/chunks/src_app_layout_tsx_2550a50e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/c6b16_next_dist_78900139._.js"
-],
-    source: "dynamic"
-});
